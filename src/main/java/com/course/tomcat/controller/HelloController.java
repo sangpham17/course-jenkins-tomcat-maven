22d1c9138d4a2e455eb46a6b327f61d921a10640
sangpham17 <86835215+sangpham17@users.noreply.github.com>
2023-06-04 17:50:00 +0700

    Update HelloController.java (#1)

--- src/main/java/com/course/tomcat/controller/HelloController.java
+++ src/main/java/com/course/tomcat/controller/HelloController.java
@@ -1,14 +1,14 @@
 package com.course.tomcat.controller;
-
+ 
 import org.springframework.web.bind.annotation.*;
-
+ 
 import java.time.LocalDateTime;
 import java.util.concurrent.ThreadLocalRandom;
-
+ 
 @RestController
 @RequestMapping("/api")
 public class HelloController {
-
+ 
     @GetMapping(value = {"", "/", "/now"})
     String hello() {
         return "Now is " + LocalDateTime.now();
@@ -18,5 +18,10 @@ public class HelloController {
     int random(){
         return ThreadLocalRandom.current().nextInt(0,1000);
     }
-
+ 
+    @GetMapping(value = "/hero")
+    String hero(){
+        return "My favorite superhero is Batman";
+    }
+ 
 }
